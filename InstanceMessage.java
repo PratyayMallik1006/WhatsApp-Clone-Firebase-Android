b0a59a5430d0e65b682a42678af319dc27b726c6
Pratyay Mallik <54743822+PratyayMallik1006@users.noreply.github.com>
2023-06-11 21:49:09 +0530

    initial upload

---
+++ InstanceMessage.java
@@ -0,0 +1,22 @@
+package com.pratyay.whatsapp;
+
+public class InstanceMessage {
+    private String message;
+    private String author;
+
+    public InstanceMessage(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public InstanceMessage() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
